906404eb5fb9e5c879f9fb92dbbaaf42b377dc3f
dread_pirate_roberts <khanhamzah142@gmail.com>
2024-07-03 23:14:18 +0530

    something

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -38,7 +38,7 @@ export const SendMoney = () => {
                                     setAmount(e.target.value);
                                 }}
                                 type="number"
-                                className="flex h-10 w-full rounded-full border border-input bg-background px-3 py-2 text-sm"
+                                className="flex text-sky-950 h-10 w-full rounded-full border border-input bg-background px-3 py-2 text-sm"
                                 id="amount"
                                 placeholder="Enter amount"
                             />
