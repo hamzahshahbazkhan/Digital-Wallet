c39a554b84cfaa8be81311ef5e6ebace4a625f12
dread_pirate_roberts <khanhamzah142@gmail.com>
2024-07-03 22:39:31 +0530

    something

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,12 @@ app.use(cors(
 ));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://digital-wallet-chi.vercel.app");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 app.use('/api/v1', rootRouter);
 app.get('/hello', (req, res) => {
