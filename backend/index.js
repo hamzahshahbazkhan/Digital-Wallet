846b615f2dd51c885e93aabc29ef1bf78b449858
dread_pirate_roberts <khanhamzah142@gmail.com>
2024-07-03 21:11:16 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [digital - wallet - chi.vercel.app],
+        methods: ["POST", "PUT", "DELETE", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 app.use('/api/v1', rootRouter);
